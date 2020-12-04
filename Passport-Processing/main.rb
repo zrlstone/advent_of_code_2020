213d753d45f1bd1bc23fee65fb06a7c85db0d41d
Zak Stone <zrlstone@gmail.com>
2020-12-04 14:46:11 +0000

    completed challenge 2

--- Passport-Processing/main.rb
+++ Passport-Processing/main.rb
@@ -1,3 +1,5 @@
+require_relative 'passport'
+
 file = File.read('input.txt')
 passports = file.split(/\n\n/)
 
@@ -14,8 +16,28 @@ valid = passports.select do |n|
   required_fields?(id_array)
 end
 
-puts valid.length
-
-
+p valid.length # 222
 
 # challenge 2
+
+passport_array = []
+
+valid.each do |passport|
+  passport_hash = {}
+  passport.scan(/(.{3}):(.\w+)/).each do |pair|
+    passport_hash[pair[0].to_sym] = pair[1]
+  end
+  passport_array << passport_hash
+end
+
+passed = passport_array.select do |h|
+  passport = Passport.new(h)
+  required_fields = %w[byr iyr eyr hgt hcl ecl pid]
+  tests = []
+  required_fields.each do |field|
+    tests << passport.send("#{field}_check")
+  end
+  tests.all?
+end
+
+puts passed.length # 140
